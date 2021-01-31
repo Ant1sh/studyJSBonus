45d934c684f4f602a6d78eb8e9a569b02e53ac6a
Anton Ivanov <antishgamer@mail.ru>
2021-01-30 22:18:59 -0400

    вторая домашка

---
+++ js/script.js
@@ -0,0 +1,19 @@
+let num = 266219;
+
+//переводим в string
+num = '266219';
+
+//задаем переменную для начала умножения при первом проходе цикла 
+let result = 1;
+
+for(let i=0; i < num.length; i++){
+  result = result * num[i];
+}
+console.log(result);
+
+//возводим результат в степень 
+let raising = result ** 3;
+console.log(raising);
+
+// Вывести на экран первые 2 цифры полученного числа
+console.log(raising.toString().substr(0,2));
