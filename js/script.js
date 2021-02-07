379ccebb4799e719efd53fa027e33402e65e4b12
Anton Ivanov <antishgamer@mail.ru>
2021-02-06 20:15:08 -0400

    домашка

--- js/script.js
+++ js/script.js
@@ -1,58 +1,18 @@
-"use strict";
+'use strict';
 
-const getRandomInt = function(max) {
-  return Math.floor(Math.random() * Math.floor(max));
-};
+const week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
 
-const isNum = function(n) {
-  return !isNaN(parseFloat(n)) && isFinite(n);
-};
+const day = new Date();
+let numWeekDay = day.getDay();
+if ( numWeekDay === 0) { numWeekDay = 6; } else { numWeekDay--; }
 
-const start = function() {
-  let randomNumber = getRandomInt(100);
-  console.log("randomNumber: ", randomNumber);
-  let numberAttempts = 10;
-  const game = function() {
-    numberAttempts--;
-    if (numberAttempts < 0) {
-      if (confirm("Попытки закончились, хотите сыграть еще?")) {
-        start();
-      } else {
-        alert("До свидания");
-        return;
-      }
+week.forEach((day, i) => {
+    let str = day;
+    if (i === numWeekDay) {
+        str = `<b>${day}</b>`;
     } else {
-      const num = prompt(
-        '"Угадай число от 1 до 100" (Для выхода оставьте строку пустой)'
-      );
-      if (num === null) {
-        alert("До свидания");
-        return;
-      }
-      if (isNum(num)) {
-        const realNum = +num;
-        if (realNum > randomNumber) {
-          alert(`Загаданное число меньше, осталось попыток: ${numberAttempts}`);
-          game();
-        } else if (realNum < randomNumber) {
-          alert(`Загаданное число больше, осталось попыток: ${numberAttempts}`);
-          game();
-        } else {
-          if (confirm("Поздравляю, Вы угадали!!! Хотели бы сыграть еще?")) {
-            start();
-          } else {
-            alert("До свидания");
-            return;
-          }
-        }
-      } else {
-        alert("Введите число");
-        game();
-      }
+        str = `${day}`;
     }
-  };
-  console.dir(game);
-  game();
-};
-
-start();
+    if (i === 5 || i === 6) { str = `<i>${str}</i>`; }
+    document.body.insertAdjacentHTML('beforeend', `<div>${str}</div>`);
+});
